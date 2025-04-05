e7ac9f10c1e200ba2caaada0986697a0e739d910
Kurtis Mok <kurtis.seven@gmail.com>
2025-04-05 16:43:08 -0700

    added graphs

--- kurtis.py
+++ kurtis.py
@@ -1,6 +1,27 @@
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
-fig, ax = plt.subplots()             # Create a figure containing a single Axes.
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.asdasd
-plt.show()                           # Show the figure.asdasd
+data = pd.read_csv(r'C:\Users\kurti\Downloads\Spotify-Songs\ignore\dataset.csv', encoding='utf-8')
+
+# scatter plot of song tempo and popularity
+'''
+# data = data[data['tempo'] > 0]
+# plt.scatter(data['tempo'], data['popularity'], alpha=0.5)
+# plt.xlabel('Tempo')
+# plt.ylabel('Popularity')
+# plt.show()
+'''
+
+# bar graph of track genres and average popularity
+'''
+average_popularity = data.groupby('track_genre')['popularity'].mean()
+
+plt.figure(figsize=(18, 6))
+average_popularity.sort_values().plot(kind='bar', color='skyblue')
+plt.title('Average Popularity of Songs by Genre')
+plt.xlabel('Genre')
+plt.ylabel('Average Popularity')
+plt.xticks(rotation=90)  
+plt.tight_layout()
+plt.show()
+'''
